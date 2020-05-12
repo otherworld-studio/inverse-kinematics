179f853d9d9b7d41b7d37f4a4a5d9c9a9f992ff8
Brian Stone <brianstone@berkeley.edu>
2020-05-11 21:57:03 -0700

    Funny walking animation

--- 184 Robot Project/Assets/Scripts/IKAnimation.cs
+++ 184 Robot Project/Assets/Scripts/IKAnimation.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public struct KeyFrame
 {
     public Transform transform;
-    public float period;//Time taken to move from this keyframe to the next
+    public float duration;//Time taken to move from this keyframe to the next
+    public bool smooth;
 }
 
 public class IKAnimation : MonoBehaviour
@@ -17,6 +18,12 @@ public class IKAnimation : MonoBehaviour
     [SerializeField]
     private List<KeyFrame> keyframes;//These are the states we will move the target object between, to make the skeleton do things
 
+    [SerializeField]
+    private int keyframe_start;
+
+    [SerializeField]
+    private float timer_start;
+
     private int cur;//This keeps track of the current keyframe
     private int next;//This keeps track of the NEXT keyframe
     private float timer;//Time since the last keyframe switch
@@ -24,17 +31,17 @@ public class IKAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cur = 0;
+        cur = keyframe_start;
         Debug.Assert(keyframes.Count > 0);
-        next = 1;
-        timer = 0f;
+        next = cur + 1;
+        timer = timer_start;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > keyframes[cur].period)
+        if (timer > keyframes[cur].duration)
         {
             cur = next;
             next = (next + 1) % keyframes.Count;
@@ -42,7 +49,10 @@ public class IKAnimation : MonoBehaviour
         }
 
         //Interpolate position and rotation
-        float t = Mathf.SmoothStep(0f, 1f, timer / keyframes[cur].period);
+        float t = timer / keyframes[cur].duration;
+        if (keyframes[cur].smooth) {
+            t = Mathf.SmoothStep(0f, 1f, t);
+        }
         Transform t0 = keyframes[cur].transform;
         Transform t1 = keyframes[next].transform;
         target.position = Vector3.Lerp(t0.position, t1.position, t);
